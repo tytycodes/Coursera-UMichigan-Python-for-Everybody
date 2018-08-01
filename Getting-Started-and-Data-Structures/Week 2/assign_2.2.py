39846c2493aeb002a70bdbe0616f1000db016d37
Ty <tycodes@protonmail.com>
2018-07-31 23:31:20 -0500

    Add files via upload

---
+++ Getting-Started-and-Data-Structures/Week 2/assign_2.2.py
@@ -0,0 +1,4 @@
+# The code below now works
+
+name = raw_input("Enter your name: ")
+print "Hello", name
---
+++ Getting-Started-and-Data-Structures/Week 2/assign_2.3.py
@@ -0,0 +1,11 @@
+# This program will compute total pay
+
+hrs = raw_input("Enter Hours:")
+rt = raw_input("Enter pay per hour:")
+
+hours = float(hrs)
+rate = float(rt)
+
+pay = hours * rate
+
+print pay
